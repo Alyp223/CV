17c8649a585eb64935c2d4cf05ade2dc689d4fbf
Alex Parsons <alexanderparsons@hotmail.com>
2023-02-04 14:11:37 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import './App.css'
+export function App() {
+  const [count, setCount] = useState(0)
+const [name, setName] = useState ("")
+
+  return (
+    <div className="App">
+      <div>
+           </div>
+      <img src="/AP cv.png" alt="my logo"/>
+      <h1>CV for Alex Parsons</h1>
+      <h2>Alex Parsons</h2>
+    <h2> 7 Dallman Close</h2>
+    <h2> Hucknall</h2>
+    <h2> NG15 6UL</h2>
+
+<table>
+  <tr>
+    <th> Company</th>
+    <th> Job title</th>
+    <th> Date Range</th>
+    <th> Duties </th>
+  </tr>
+  <tr> 
+    <td> Nottingham Trent University</td>
+    <td> International Marketing Assistant</td>
+    <td> 05/10/2020 - Current </td>
+    <td> Website management, social media, 
+      content production, video editing, digital training</td>
+  </tr>
+  <tr>
+    <td> University of Derby</td>
+    <td> Marketing Officer</td>
+    <td>23/04/2018-26/07/2019</td>
+    <td>Website management, email campaigns, social media content, marketing reporting</td>
+    </tr>
+    <tr> 
+     <td> University of Derby</td>
+      <td> Marketing Intern </td>
+      <td> 13/11/2017-20/04/2018</td>
+      <td> Creating designs, briefing in marketing projects, monitoring the website</td>
+    </tr> 
+    <tr> 
+    <td> YES Ltd.</td>
+    <td> Marketing Assistant</td>
+    <td>11/05/2016-28/04/2017 </td>
+    <td> SEO management, Google Adwords, blogposts, PR, graphic design </td>
+    </tr>
+ 
+</table>    
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <input value={name} onChange={(event)=> setName (event.target.value)}/>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+
+
